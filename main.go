3238a7b63c53a2ae8b22d9ecc2e2dfbadc766e8f
kirkbyers <kirklbyers@gmail.com>
2018-07-28 16:38:46 -0500

    Init bolt db. Add CreateTask.

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/kirkbyers/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/kirkbyers/task/cmd"
+	"github.com/kirkbyers/task/db"
+	homedir "github.com/mitchellh/go-homedir"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, err := homedir.Dir()
+	must(err)
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
