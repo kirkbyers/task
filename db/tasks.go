9c9d4b8ed449bed68b0c1483ef0042c14408dcba
kirkbyers <kirklbyers@gmail.com>
2018-07-28 16:49:19 -0500

    Add DeleteTask

--- db/tasks.go
+++ db/tasks.go
@@ -65,6 +65,15 @@ func AllTasks() ([]Task, error) {
 	return tasks, nil
 }
 
+// DeleteTask removes a task at key
+func DeleteTask(key int) error {
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		return b.Delete(itob(key))
+	})
+	return err
+}
+
 func itob(v int) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(v))
